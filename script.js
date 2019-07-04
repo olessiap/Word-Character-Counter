27272025859c647b8701ce293d51aaa5b60cd6ea
olessiap <olessia.potapova@gmail.com>
2019-07-04 16:35:29 -0300

    used event bubbling to listen to events on textarea, word count is updating correctly

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-let characterCount = 0;
+let charNum = document.getElementById("character-count");
+let wordNum = document.getElementById("word-count");
 
-let userInput = document.querySelector('textarea');
-console.log(userInput.textContent.length);
+let inputHandler = function(event) {
+    if (!event.target.matches('[data-character-count]')) return;
+    let wordCount = event.target.value.split(/[\n\r\s]+/g).filter(function(word) {
+        return word.length > 0;
+    });
+    wordNum.textContent = wordCount.length;
+    charNum.innerHTML = event.target.value.length
 
-userInput.addEventListener('input', function(event) {
-    console.log(event.target.value.length);
-    let charNum = document.getElementById("character-count");
-    charNum.innerHTML = event.target.value.length;
-})
+    
+}
+
+document.addEventListener('input', inputHandler, false);
